6c320cec688cf354ba99427e315ec17bcd466906
Carlos Marques <carlos.marques@pretrab.local>
2026-05-28 22:51:05 +0100

    Corrigir caminho do logger no scheduler

--- core/scheduler.js
+++ core/scheduler.js
@@ -5,7 +5,7 @@
 
 const cron = require("node-cron");
 const { sendWeeklyReport } = require("./weeklyReport");
-const { log } = require("./logger");
+const { log } = require("../services/logger");
 
 // Executa todas as segundas às 08:00
 // Formato CRON: "minuto hora diaMes mes diaSemana"
@@ -13,3 +13,4 @@ cron.schedule("0 8 * * 1", () => {
     log("📅 Executando relatório semanal (Segunda 08:00)...");
     sendWeeklyReport();
 });
+
